153fbc245983dca72f81bc1bf91dad7195bb9919
macbook <harisgorcevic1000@gmail.com>
2023-11-28 22:24:53 +0100

    Rad

--- src/Home/HomePage.js
+++ src/Home/HomePage.js
@@ -4,14 +4,12 @@ import "./homepage.css";
 import Card from "../Components/Card";
 import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
-
+import { Search } from "@mui/icons-material";
 export default function HomePage() {
   const [data, setData] = useState([]);
   const [page, setPage] = useState(0);
-  const [secondData, setSecondData] = useState([]);
-  const [money, setMoney] = useState([]);
-  const [a, setA] = useState([]);
-  const [secondMoney, setSecondMoney] = useState([]);
+  const [search, setSearch] = useState(0);
+  const [selectedCurrency, setSelectedCurrency] = useState("USD");
 
   const handlePageChange = (event, newPage) => {
     setPage(newPage);
@@ -32,6 +30,7 @@ export default function HomePage() {
           orderDirection: "desc",
           limit: "50",
           offset: page,
+          query: search,
         },
         headers: {
           "X-RapidAPI-Key":
@@ -42,75 +41,37 @@ export default function HomePage() {
       .then((response) => setData(response.data.data.coins));
   }
 
-  function getUsd() {
-    axios
-      .get(`https://coingecko.p.rapidapi.com/coins/bitcoin`, {
-        params: {
-          localization: "true",
-          tickers: "true",
-          market_data: "true",
-          community_data: "true",
-          developer_data: "true",
-          sparkline: "false",
-        },
-        headers: {
-          "X-RapidAPI-Key":
-            "1b2013684fmsh5e2154cde374d29p1987b9jsnf9a0e60af14e",
-          "X-RapidAPI-Host": "coingecko.p.rapidapi.com",
-        },
-      })
-      .then((response) => {
-        {
-          // setSecondData(response.data.market_data.current_price);
-          const mappedArray = Object.entries(
-            response.data.market_data.current_price
-          );
-          setSecondData(mappedArray);
-          // let array = Object.values(response.data.market_data.current_price);
-          // console.log(array);
-          // setMoney(array);
-          // let keys = Object.keys(response.data.market_data.current_price);
-          // setSecondMoney(keys);
-          // setA(response.data.market_data.current_price);
-        }
-      });
-  }
-  console.log(a);
-  console.log(secondMoney);
-  console.log(money);
   useEffect(() => {
     getCoins();
-    getUsd();
-  }, [page]);
-  console.log(data);
-  console.log(secondData);
-  console.log("object");
+  }, [page, search]);
+
   return (
     <div className="container">
-      <select>
-        {secondData.map(([el, index]) => (
-          <option value={index}>{el}</option>
-        ))}
-      </select>
-
+      <button
+        onClick={() =>
+          setSelectedCurrency(selectedCurrency === "USD" ? "EUR" : "USD")
+        }
+      >
+        {selectedCurrency === "USD" ? "Prikaži u EUR" : "Prikaži u USD"}
+      </button>
       {data.map((product) => (
-        <Card coin={product} />
+        <div className="card">
+          <img src={product.iconUrl} alt={product.name} />
+          <h3>{product.name}</h3>
+          <p style={{ fontWeight: "bold" }}>
+            {selectedCurrency === "USD"
+              ? `$ ${parseFloat(product.price).toFixed(1)}`
+              : `€ ${parseFloat(product.price * 0.85).toFixed(1)}`}
+          </p>
+        </div>
       ))}
-      <Stack spacing={2}>
-        <Pagination
-          sx={{ paddingTop: "20px" }}
-          onChange={handlePageChange}
-          count={644}
-          variant="outlined"
-          shape="rounded"
-        />
-      </Stack>
+      <Pagination
+        sx={{ paddingTop: "20px" }}
+        onChange={handlePageChange}
+        count={644}
+        variant="outlined"
+        shape="rounded"
+      />
     </div>
   );
 }
-
-// [
-//   ['key1', 'value1'],
-//   ['key2', 'value2'],
-//   ['key3', 'value3']
-// ]/OVO JE zapravo Object.entries on pristupa i key i value key je element a value je index
