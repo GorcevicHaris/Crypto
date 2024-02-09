0d8f9544da27942fddfd5e51e146be36d626c3b7
macbook <harisgorcevic1000@gmail.com>
2024-02-10 00:01:45 +0100

    Rad

--- src/Chart/ChartPage.js
+++ src/Chart/ChartPage.js
@@ -2,8 +2,7 @@ import "./chartpage.css";
 import React, { useContext, useEffect, useState, useRef } from "react";
 import { CartContext } from "../Context/Context";
 import { BarChart } from "@mui/x-charts/BarChart";
-import axios from "axios";
-import { Line } from "react-chartjs-2";
+import { Line, Doughnut } from "react-chartjs-2";
 import { useLocation } from "react-router-dom";
 import {
   Chart as ChartJS,
@@ -12,7 +11,14 @@ import {
   LinearScale,
   PointElement,
 } from "chart.js";
-ChartJS.register(LineElement, PointElement, LinearScale, CategoryScale);
+import { Chart, ArcElement } from "chart.js";
+ChartJS.register(
+  LineElement,
+  PointElement,
+  LinearScale,
+  CategoryScale,
+  ArcElement
+);
 
 export default function ChartPage(product) {
   const { state } = useLocation();
@@ -41,18 +47,35 @@ export default function ChartPage(product) {
     ],
     datasets: [
       {
-        label: "Line Dataset",
+        label: "Bar Dataset",
         data: state.sparkline,
         lineTension: 0.5,
         borderColor: "green",
         borderWidth: 1.5,
-        pointRadius: 2,
+        pointRadius: 0,
         width: 300,
         height: 300,
+        order: 2,
+        data: state.sparkline,
         showLine: true,
       },
     ],
   };
+  const data = {
+    labels: ["monday", "friday", "sutarday", "asdas"],
+    datasets: [
+      {
+        data: [1, 2, 3, 4],
+        backgroundColor: [
+          "rgb(255, 99, 132)",
+          "rgb(54, 162, 235)",
+          "rgb(255, 205, 86)",
+        ],
+        hoverOffset: 4,
+      },
+    ],
+  };
+
   console.log(state, "state");
   return (
     <div className="chartContainer">
@@ -67,14 +90,11 @@ export default function ChartPage(product) {
                 ticks: { stepSize: 3 },
               },
             },
-            elements: {
-              line: {
-                tension: 1,
-              },
-            },
+            elements: {},
           }}
         />
       </div>
+      <Doughnut data={data} />
     </div>
   );
 }
